06d1c101418bccf9bceafc5f71f82ccdeb218cf1
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-10-14 15:42:30 +0200

    🔨 Implement initial app structure and PDF text extraction functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,50 @@
+import { useState, useEffect } from "react";
+import constants, { buildPresenceChecklist, METRIC_CONFIG } from "../constants";
+import * as pdfjsLib from "pdfjs-dist";
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker.min?url";
+pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 function App() {
+  const [aiReady, setAiReady] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [uploadedFIle, setUploadedFIle] = useState(null);
+  const [analysis, setAnalysis] = useState(null);
+  const [resumeText, setResumeText] = useState("");
+  const [presenceChecklist, setPresenceChecklist] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (window.puter?.ai?.chat) {
+        setAiReady(true);
+        clearInterval(interval);
+      }
+    }, 300);
+    return () => clearInterval(interval);
+  }, []);
+
+  const extractPDFText = async (file) => {
+    const arrayBuffer = await file.arrayBuffer();
+    const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+    const texts = await Promise.all(
+      Array.from({ length: pdf.numPages }, (_, i) => pdf.getPage(i + 1)).then(
+        (page) =>
+          page
+            .getTextContent()
+            .then((tc) => tc.items.map((i) => i.str).join(" "))
+      )
+    );
+    return texts.join("\n").trim();
+  };
+
+  const parseJSONResponse = (reply) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+
   return (
     <div className="min-h-screen bg-main-gradient p-4 sm:p-6 lg:p-8 flex items-center justify-center">
       <h1 className="text-7xl text-white">AI RESUME ANALYZER</h1>
